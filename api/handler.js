fc26f059bddfc7e4d76fe57733756fedef49135c
Schalk Neethling <schalk.neethling@mechanical-ink.co.za>
2022-12-28 01:46:16 +0200

    add logging

--- api/handler.js
+++ api/handler.js
@@ -2,6 +2,7 @@ import fetch from "node-fetch";
 
 export default async function handler(request, response) {
   const { breed } = request.query;
+  console.log(request.query, breed);
   const apiResponse = await fetch(
     `https://dog.ceo/api/breed/${breed}/images/random`
   );
