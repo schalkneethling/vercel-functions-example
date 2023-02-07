5c80179c11dd4f32b636226ce427f6fe7ebc15ee
Schalk Neethling <schalk.neethling@mechanical-ink.co.za>
2023-02-07 17:25:56 +0200

    Update handler.js

--- api/update-settings/handler.js
+++ api/update-settings/handler.js
@@ -1,17 +1,3 @@
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-
-  return await fn(req, res)
-}
-
 export default async function handler(request, response) {
   return response.status(200).json({ success: true });
 }
-
-module.exports = allowCors(handler)
